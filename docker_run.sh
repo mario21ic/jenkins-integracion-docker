bc7e17ae8a506d333c82c6feec72afde13b18363
tuxito <tuxito@profesor.aulautil.com>
2019-12-07 17:27:48 -0500

    adding errors

--- docker_run.sh
+++ docker_run.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
+
+mkdir build/
+
 docker run node:7-alpine node --version
 docker run node:6-alpine node --version
 docker run -v $HOME/.m2:/root/.m2 -v $WORKSPACE:/apps/ maven:3-alpine ls -la /apps
+
+mkdirs build/
 
